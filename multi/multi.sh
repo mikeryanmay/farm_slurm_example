e5f91d030a073149233ce4e09bb6a0b067f61f67
Mike <mikeryanmay@gmail.com>
2021-08-23 16:32:44 -0700

    multiple cores

---
+++ multi/multi.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#SBATCH --partition=low2
+#SBATCH --account=brannalagrp
+#SBATCH --output=/home/%u/farm_slurm_example/multi/simple-stdout-%j.out
+#SBATCH --error=/home/%u/farm_slurm_example/multi/simple-stderr-%j.out
+#SBATCH --job-name=multi_example
+#SBATCH --mail-user=mikeryanmay@gmail.edu
+#SBATCH --mail-type=ALL
+#SBATCH --time=00:00:30
+#SBATCH --ntasks=20
+
+# change to user directory
+# cd /home/$USER/farm_slurm_example/multi/
+
+# run your code
+mkdir -p output
+
+writeTime () {
+	echo $(date +"%T") > "output/time_$1.txt"
+	sleep 2
+}
+export -f writeTime
+
+parallel -j $SLURM_CPUS_ON_NODE writeTime ::: {1..20}
