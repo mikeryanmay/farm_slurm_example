e0768ccaa51a61b3238497413d0e87f793099a76
Mike <mikeryanmay@gmail.com>
2021-08-23 13:02:28 -0700

    ignoring any output

--- simple/simple.sh
+++ simple/simple.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 #SBATCH --partition=low2
 #SBATCH --account=brannalagrp
-#SBATCH --output=/home/%u/farm_slurm_example/simple/simple-stdout-%j.txt
-#SBATCH --error=/home/%u/farm_slurm_example/simple/simple-stderr-%j.txt
+#SBATCH --output=/home/%u/farm_slurm_example/simple/simple-stdout-%j.out
+#SBATCH --error=/home/%u/farm_slurm_example/simple/simple-stderr-%j.out
 #SBATCH --job-name=simple_example
 #SBATCH --mail-type=ALL
 #SBATCH --time=00:00:30
 #SBATCH --ntasks=1
 
 # change to user directory
-cd /home/$USER/farm_slurm_example/simple/
+# cd /home/$USER/farm_slurm_example/simple/
 
 # run your code
-echo "Hello World"
+mkdir output
+echo "Hello World" > output/simple.txt
