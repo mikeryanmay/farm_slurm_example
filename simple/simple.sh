dd96397b382824bc00f22976a2894ef3a9720bc0
Mike <mikeryanmay@gmail.com>
2021-08-23 12:48:39 -0700

    changing output files

--- simple/simple.sh
+++ simple/simple.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 #SBATCH --partition=low2
 #SBATCH --account=brannalagrp
-#SBATCH --output=/home/%u/farm_slurm_example/simple/slurm_log/simple-stdout-%j.txt
-#SBATCH --error=/home/%u/farm_slurm_example/simple/slurm_log/simple-stderr-%j.txt
 #SBATCH --job-name=simple_example
 #SBATCH --time=00:00:30
 #SBATCH --ntasks=1
